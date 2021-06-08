a5b985af34754a3f40c07f211b73fd78e448ebca
K-Rintaro <k.rintaro1@icloud.com>
2021-06-08 22:33:21 +0900

    base code

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const ping = require('ping');
+const chalk = require('chalk');
 const fs = require('fs');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 
+
+fs.readFile('./test_public/index.html', 'utf-8', (err, data) => {
+    if(data) {
+        const dom = new JSDOM(data)
+        console.log(dom.window.document.querySelector("script").src);
+    }else{
+        console.log(err);
+    }
+});
