f277e129dda20b11264130242105eaefdeababd0
K-Rintaro <k.rintaro1@icloud.com>
2021-06-10 22:02:33 +0900

    link cdn complete

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const { JSDOM } = jsdom;
 const url = require('url');
 
 const kakunouko = []
-const test = ["www.rintaro.tech/aaa"]
+const kakunoukosub = []
 
 fs.readFile('./test_public/index.html', 'utf-8', (err, data) => {
     if (data){
         console.log(chalk.greenBright("[CDNCovering] Running now..."))
         const dom = new JSDOM(data)
         const jsnum = dom.window.document.querySelectorAll('script').length
+        const linknum = dom.window.document.querySelectorAll('link').length
         if (!jsnum == 0){
             for(let i=0; jsnum > i; i++){
                 var maindish = dom.window.document.querySelectorAll('script')[i].src;
@@ -21,6 +22,26 @@ fs.readFile('./test_public/index.html', 'utf-8', (err, data) => {
                     kakunouko.push(kakunou)
                     console.log(chalk.green("CDN found: ") + chalk.white(maindish))
                     kakunouko.forEach(function(host){
+                        ping.sys.probe(host, function(isAlive){
+                            var msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
+                            if (msg.includes("alive")){
+                                console.log(chalk.blueBright("✔︎" + msg))
+                            }else if(msg.includes("dead")){
+                                console.log(chalk.redBright("△" + msg))
+                            }
+                        });
+                    });
+                }
+            }
+        }
+        if (!linknum == 0){
+            for(let i=0; linknum > i; i++){
+                var subdish = dom.window.document.querySelectorAll('link')[i].href;
+                if (maindish.startsWith('htt')){
+                    var linkkakunou = url.parse(subdish).hostname;
+                    kakunoukosub.push(linkkakunou)
+                    console.log(chalk.green("CDN found: ") + chalk.white(subdish))
+                    kakunoukosub.forEach(function(host){
                         ping.sys.probe(host, function(isAlive){
                             var msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
                             if (msg.includes("alive")){
