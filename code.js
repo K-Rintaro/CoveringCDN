2a5551e0f4bb4fa458350c85d34d1ab95851ceae
K-Rintaro <k.rintaro1@icloud.com>
2021-06-11 22:30:01 +0900

    considering null

--- code.js
+++ code.js
@@ -5,8 +5,9 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const url = require('url');
 const { exec } = require('child_process')
+const client = require('cheerio-httpcli');
 
-const CDNCheckingLocal = function(path, command) {
+const local = function(path, command) {
     const kakunouko = []
     const kakunoukosub = []
     
@@ -77,4 +78,82 @@ const CDNCheckingLocal = function(path, command) {
             console.log(chalk.red("[CDNCovering] Error: ") + chalk.yellow(err))
         }
     })
+}
+
+const remote = function(uri, command){
+    const kakunouko = []
+    const kakunoukosub = []
+    
+    client.fetch(uri, function(err, $, res, body){
+        if (body?.includes('<!DOCTYPE html>')){
+            console.log(chalk.greenBright("[CDNCovering] Running now..."))
+            const dom = new JSDOM(body)
+            const jsnum = dom.window.document.querySelectorAll('script').length
+            const linknum = dom.window.document.querySelectorAll('link').length
+            if (!jsnum == 0){
+                for(let i=0; jsnum > i; i++){
+                    var maindish = dom.window.document.querySelectorAll('script')[i].src;
+                    if (maindish.startsWith('htt')){
+                        var kakunou = url.parse(maindish).hostname;
+                        kakunouko.push(kakunou)
+                        console.log(chalk.green("CDN found: ") + chalk.white(maindish))
+                        kakunouko.forEach(function(host){
+                            ping.sys.probe(host, function(isAlive){
+                                var msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
+                                if (msg.includes("alive")){
+                                    console.log(chalk.blueBright("✔︎" + msg))
+                                }else if(msg.includes("dead")){
+                                    console.log(chalk.redBright("△" + msg))
+                                    exec(command, (err, stdout, stderr) => {
+                                        if (err) {
+                                          console.log(`stderr: ${stderr}`)
+                                          return
+                                        }
+                                        console.log(`stdout: ${stdout}`)
+                                      }
+                                    )
+                                }
+                            });
+                        });
+                    }
+                }
+            }
+            if (!linknum == 0){
+                for(let i=0; linknum > i; i++){
+                    var subdish = dom.window.document.querySelectorAll('link')[i].href;
+                    if (subdish.startsWith('htt')){
+                        var linkkakunou = url.parse(subdish).hostname;
+                        kakunoukosub.push(linkkakunou)
+                        console.log(chalk.green("CDN found: ") + chalk.white(subdish))
+                        kakunoukosub.forEach(function(host){
+                            ping.sys.probe(host, function(isAlive){
+                                var msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
+                                if (msg.includes("alive")){
+                                    console.log(chalk.blueBright("✔︎" + msg))
+                                }else if(msg.includes("dead")){
+                                    console.log(chalk.redBright("△" + msg))
+                                    exec(command, (err, stdout, stderr) => {
+                                        if (err) {
+                                          console.log(`stderr: ${stderr}`)
+                                          return
+                                        }
+                                        console.log(`stdout: ${stdout}`)
+                                      }
+                                    )
+                                }
+                            });
+                        });
+                    }
+                }
+            }
+        }else{
+            console.log(chalk.red("[CDNCovering] Error: ") + chalk.yellow(err))
+        }
+    })
+    
+}
+
+module.exports = {
+    local,
+    remote
 }
---
+++ index.js
@@ -0,0 +1,3 @@
+const CDNCovering = require("./code.js")
+
+CDNCovering.remote('https://www.rintaro.tech', 'date')
